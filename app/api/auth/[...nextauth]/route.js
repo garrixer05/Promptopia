f2a526c014df214f327d904ea61d593ad3b44e53
garrixer05 <aa20494@gmail.com>
2023-11-26 01:21:32 +0530

    First commit

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,42 @@
+import User from "@models/user";
+import { connectDb } from "@utils/database";
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+const handler = NextAuth({
+    providers : [
+        GoogleProvider({
+            clientId : process.env.GOOGLE_CLIENT_ID,
+            clientSecret : process.env.GOOGLE_CLIENT_SECRET
+        })
+    ],
+    async session ({session}){
+        const sessionUser = await User.findOne({
+            email : session.user.email
+        })
+        session.user.id = sessionUser._id.toString();
+        return session;
+    },
+    async signIn ({profile}){
+        try {
+            //serverless route
+            connectDb();
+            // check user already exist
+            const userExist = await User.findOne({
+                email:profile.email
+            });
+            if(!userExist){
+                await User.create({
+                    email :profile.email,
+                    username : profile.name.replace(" ",""),
+                    image : profile.picture
+                })
+            }
+            return true;
+        } catch (error) {
+            console.log(error);
+            return false
+        }
+    }
+})
+
+export {handler as GET, handler as POST};

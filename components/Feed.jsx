bf2523a65e897505010ad131c5c5d586e3d2d3d5
garrixer05 <aa20494@gmail.com>
2024-03-20 18:58:45 +0530

    fix

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -15,9 +15,39 @@ const PromptCardList = ({ data, handleTagClick }) => {
     </div>
   );
 };
+const obj = {
+  creators: {
+    id: [],
+    username: [],
+    email: [],
+  },
+  tag: {
+    id: [],
+    tagArr: [],
+  },
+  prompts: {
+    id: [],
+    promptArr: [],
+  },
+};
 
 const Feed = () => {
   const [searchText, setsearchText] = useState("");
+  const [forSearch, setforSearch] = useState({
+    creators: {
+      id: [],
+      username: [],
+      email: [],
+    },
+    tag: {
+      id: [],
+      tagArr: [],
+    },
+    prompts: {
+      id: [],
+      promptArr: [],
+    },
+  });
   const [posts, setposts] = useState([]);
 
   useEffect(() => {
@@ -28,10 +58,41 @@ const Feed = () => {
     };
     fetchPosts();
   }, []);
-  const handleSearchChange = (e) => {};
+
+  const fillSearch = () => {
+    let N = obj;
+    console.log(obj);
+    for (let post of posts) {
+      N.creators.id.push(post.creator._id);
+      N.creators.username.push(post.creator.username);
+      N.creators.email.push(post.creator.email);
+      N.tag.id.push(post._id);
+      N.tag.tagArr.push(post.tag);
+      N.prompts.id.push(post._id);
+      N.prompts.promptArr.push(post.prompt);
+    }
+    setforSearch((prev) => {
+      return (prev = N);
+    });
+  };
+
+  const handleSearchChange = (e) => {
+    fillSearch();
+    e.preventDefault();
+    setsearchText(e.target.value);
+    setTimeout(() => {
+      if (searchText) {
+      }
+    }, 1000);
+  };
+
   return (
     <section className="feed">
-      <form className="relative w-full flex-center" action="">
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        className="relative w-full flex-center"
+        action=""
+      >
         <input
           placeholder="Search for a tag or username"
           value={searchText}
@@ -45,5 +106,4 @@ const Feed = () => {
     </section>
   );
 };
-
 export default Feed;

23d71ed47d4083860705178fcf2a9f39a485a164
garrixer05 <aa20494@gmail.com>
2024-03-22 17:50:15 +0530

    fix

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,29 +1,28 @@
-import '@styles/globals.css';
-import Nav from '@components/Nav';
+import "@styles/globals.css";
+import Nav from "@components/Nav";
 import Provider from "@components/Provider";
 
-
 export const metadata = {
-    title : "promptopia",
-    description : "Discover & share AI prompts"
-}
+  title: "Promptopia",
+  description: "Discover & share AI prompts",
+};
 
-const RootLayout = ({children}) => {
+const RootLayout = ({ children }) => {
   return (
-    <html lang='en'>
-        <body>
-            <Provider>
-                <div className='main'>
-                    <div className='gradient'/>
-                </div>
-                <main className='app'>
-                    <Nav/>
-                    {children}
-                </main>
-            </Provider>
-        </body>
+    <html lang="en">
+      <body>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
+      </body>
     </html>
-  )
-}
+  );
+};
 
 export default RootLayout;
